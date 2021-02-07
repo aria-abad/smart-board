b5a9c99183f93e2119d3992fcf196e2c1a83e845
Aria <aria.abad2030@yahoo.com>
2021-02-07 15:29:25 -0800

    Updates

--- static/js/main.b19c7d7b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsmart-board"]=this["webpackJsonpsmart-board"]||[]).push([[0],{11:function(t,e,r){"use strict";r.r(e);var c=r(1),n=r.n(c),a=r(3),s=r.n(a),o=(r(8),r(9),r(0));var i=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"React Smart Board Project"})})};s.a.render(Object(o.jsx)(n.a.StrictMode,{children:Object(o.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,r){},9:function(t,e,r){}},[[11,1,2]]]);
-//# sourceMappingURL=main.b19c7d7b.chunk.js.map
