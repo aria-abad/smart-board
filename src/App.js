914ff95b75e15784f0b0d97e4f9e2d2b02f05cfb
Aria <aria.abad2030@yahoo.com>
2021-02-08 18:46:09 -0800

    add state to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [board, setBoard] = useState([
+    {pCyle: "SSU28", instrumentType: "Cadiere x4", autoClaveCycle: "WO", completionTime:"W", contactPerson: "Halie"},
+    {pCyle: "SSU12", instrumentType: "Refurb MSCND 13 - 22", autoClaveCycle: "WO", completionTime:"", contactPerson: "nICK"}
+  ])
+
   return (
     <div className="App">
       <h1>React Smart Board Project</h1>
