7c8dfc448eba36a9156de5dd5142401ddc188bd0
Aria <aria.abad2030@yahoo.com>
2021-02-09 21:14:20 -0800

    update the navbar title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <h1>NPV Lab Projects</h1>
+            <h1>N Lab Projects</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Project</a>
