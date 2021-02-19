fc47b909302665d7b35cf459ed0db15586c52700
Aria <aria.abad2030@yahoo.com>
2021-02-18 20:00:57 -0800

    Add json server

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import {v4 as uuidv4} from 'uuid';
 function App() {
 
   const [projects, setProjects] = useState([
-    {id: uuidv4(), priority: 3, cycle: 'SSU28', instrument: 'C x4', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'with owner', contact:'Halie'},
+    // {id: uuidv4(), priority: 3, cycle: 'SSU28', instrument: 'C x4', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'with owner', contact:'Halie'},
     {id: uuidv4(), priority: 1, cycle: 'SSU2', instrument: 'Xi T 1-6 (Green)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'', contact:'Aparna'},
     {id: uuidv4(), priority: 2, cycle: '(4)/5', instrument: 'S (Gen 11)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'waiting for pickup', contact:'David M.'}
   ])
 
+  // function handleDelete(id) {}
   const handleDelete = (id) => {
     const newProjects = projects.filter(project => project.id !== id);
     setProjects(newProjects);
