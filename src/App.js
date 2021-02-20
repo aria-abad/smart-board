d467d89246ddd3c601816da221d19ab9e40aec80
Aria <aria.abad2030@yahoo.com>
2021-02-20 14:00:12 -0800

    start fetch data from json file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
-import {v4 as uuidv4} from 'uuid';
+// import {v4 as uuidv4} from 'uuid';
 
 function App() {
 
-  const [projects, setProjects] = useState([
-    {id: uuidv4(), priority: 3, cycle: 'SSU28', instrument: 'C x4', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'with owner', contact:'Halie'},
-    {id: uuidv4(), priority: 1, cycle: 'SSU2', instrument: 'Xi T 1-6 (Green)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'', contact:'Aparna'},
-    {id: uuidv4(), priority: 2, cycle: '(4)/5', instrument: 'S (Gen 11)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'waiting for pickup', contact:'David M.'}
-])
+  const [projects, setProjects] = useState(null);
+  // ([
+    // {id: uuidv4(), priority: 3, cycle: 'SSU28', instrument: 'C x4', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'with owner', contact:'Halie'},
+    // {id: uuidv4(), priority: 1, cycle: 'SSU2', instrument: 'Xi T 1-6 (Green)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'', contact:'Aparna'},
+    // {id: uuidv4(), priority: 2, cycle: '(4)/5', instrument: 'S (Gen 11)', autoclave: 'WO', completion: {soak: '--', medisafe:'--', autoclave:'--'}, note:'waiting for pickup', contact:'David M.'}
+  // ])
 
   // function handleDelete(id) {}
   const handleDelete = (id) => {
@@ -17,11 +18,21 @@ function App() {
     setProjects(newProjects);
   }
 
+  useEffect(() => {
+    fetch('http://localhost:8000/projects')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        console.log(data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home projects = {projects} handleDelete={handleDelete} />
+        {/* <Home projects = {projects} handleDelete={handleDelete} /> */}
       </div>
     </div>
   );
