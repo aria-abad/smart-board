cc5ea0d81a8ebb9ed6ab0898cad1e1f6df1f1abf
Aria <aria.abad2030@yahoo.com>
2021-02-20 20:47:26 -0800

    router exact match and link update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className="navbar">
             <h1>NPV Lab Projects</h1>
             <div className="links">
-                <a href="/">Projects</a>
-                <a href="/create">New Project</a>
+                <Link to="/">Projects</Link>
+                <Link to="/create">New Project</Link>
             </div>
         </nav>
     )
---
+++ src/components/NewProject.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewProject() {
+    return (
+        <div className="new-project">
+            <h2>Add a New Project</h2>
+        </div>
+    )
+}
