361518f6d877743a89ff8813c4e056a886869e6b
Aria <aria.abad2030@yahoo.com>
2021-02-21 17:49:54 -0800

    update the project details page and the css

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import useFetch from './useFetch';
 
 export default function ProjectDetails() {
     const { id } = useParams();
+    const { data: project, error, isLoadin } = useFetch('http://localhost:8000/projects/' + id);
 
     return (
         <div className="project-details">
-            <h2>Project Details - { id }</h2>
+            {isLoadin && <div>Loading...</div>}
+            {error && <div>{ error }</div>}
+            {project && (
+                <article>
+                    <h2>{ project.instrument }</h2>
+                    <h3>SSU Cycle: { project.cycle }</h3>
+                    <h3>Autoclave: { project.autoclave }</h3>
+                    <p>Contact Person: { project.contact }</p>
+                    <div>Note: { project.note }</div>
+                </article>
+            )}
         </div>
     )
 }
