ff47368c316d1d4d888e9a21bc345c53155fb5ce
Aria <aria.abad2030@yahoo.com>
2021-02-24 20:18:34 -0800

    preparing the table to show in the main window

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom'
 
 export default function ProjectList({ projects }) {
     return (
-        <div className="project-list">
-            {projects.map(project => (
-                <div className="project-preview" key={project.id}>
-                    <Link to={`/projects/${project.id}`}>
-                        <h3>{ project.instrument }</h3>
-                    </Link>
-                    <p>Priority of this project: { project.priority}</p>
-                    <p>Process Cycle: { project.cycle }</p>
-                    <p>Autoclave Cycle: { project.autoclave }</p>
-                </div>
-            ))}
+        // <div className="project-list">
+        //     {projects.map(project => (
+        //         <div className="project-preview" key={project.id}>
+        //             <Link to={`/projects/${project.id}`}>
+        //                 <h3>{ project.instrument }</h3>
+        //             </Link>
+        //             <p>Priority of this project: { project.priority}</p>
+        //             <p>Process Cycle: { project.cycle }</p>
+        //             <p>Autoclave Cycle: { project.autoclave }</p>
+        //         </div>
+        //     ))}
+        // </div>
+        <div>
+            
         </div>
     )
 }
